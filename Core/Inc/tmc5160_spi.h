e7cd37652db768b74bb49154d1421d2074bdfa28
ncardona <ncardona@student.unimelb.edu.au>
2023-02-20 16:55:39 +1100

    Initial commit. Developing TMC5160 drivers.

---
+++ Core/Inc/tmc5160_spi.h
@@ -0,0 +1,18 @@
+/*
+ * tmc5160_spi.h
+ *
+ *  Created on: 20 Feb. 2023
+ *      Author: nelson
+ */
+
+#ifndef INC_TMC5160_SPI_H_
+#define INC_TMC5160_SPI_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
+
+#endif /* INC_TMC5160_SPI_H_ */
